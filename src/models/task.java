ee7dfde499d84d8193b2fba04b7b5f2ff4308716
ryusei.tanaka <ryusei.tanaka@axas-japan.co.jp>
2022-05-25 21:40:12 +0900

    getAllMessagesを追加

--- src/models/task.java
+++ src/models/task.java
@@ -14,12 +14,12 @@ import javax.persistence.Table;
 @Entity
 @NamedQueries({
     @NamedQuery(
-        name = "getAllMessages",
-        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+        name = "getAllTasks",
+        query = "SELECT m FROM task AS m ORDER BY m.id DESC"
     )
 })
-@Table(name = "messages")
-public class Message {
+@Table(name = "tasks")
+public class task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
